2d541c1bd4112bf97f05eafecd1abcafd44cbf2a
William F. Broderick <billbrod@gmail.com>
2019-10-07 12:00:25 -0700

    adds model name to experiment saved outputs

--- foveated_metamers/experiment.py
+++ foveated_metamers/experiment.py
@@ -385,10 +385,11 @@ def expt(stimuli_path, subj_name, sess_num, output_dir="data/raw_behavioral", ey
         binocular_offset = csv_to_binocular_offset(ipd_csv, subj_name)
     else:
         binocular_offset = [0, 0]
-    save_path = op.join(output_dir, "%s_%s_sess{sess:02d}.hdf5" %
-                        (datetime.datetime.now().strftime("%Y-%b-%d"), subj_name))
-    edf_path = op.join(output_dir, "%s_%s_sess{sess:02d}.EDF" %
-                       (datetime.datetime.now().strftime("%Y-%b-%d"), subj_name))
+    model_name = op.dirname(stimuli_path)
+    save_path = op.join(output_dir, "%s_%s_%s_sess{sess:02d}.hdf5" %
+                        (datetime.datetime.now().strftime("%Y-%b-%d"), subj_name, model_name))
+    edf_path = op.join(output_dir, "%s_%s_%s_sess{sess:02d}.EDF" %
+                       (datetime.datetime.now().strftime("%Y-%b-%d"), subj_name, model_name))
     idx_path = stimuli_path.replace('_stimuli.npy', '_idx_sess-%02d.npy' % sess_num)
     idx_path = idx_path.replace('e0', '%s_e0' % subj_name)
     save_path = save_path.format(sess=sess_num)
