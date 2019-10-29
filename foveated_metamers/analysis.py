bad29c6f4f7d575e04293387b048b334ac96a975
William F. Broderick <billbrod@gmail.com>
2019-10-29 16:28:46 -0700

    updates analysis
    it's more helpful now. functions have been added to create an
    "experiment_df", which has a row per trial, with the necessary info
    there, to modify that df to add information about the subject's
    responses and whether they were correct or not, and to average within
    each condition of that df to get the proportion correct

--- foveated_metamers/analysis.py
+++ foveated_metamers/analysis.py
@@ -2,6 +2,7 @@
 """
 import h5py
 import numpy as np
+import pandas as pd
 
 
 def summarize_trials(raw_behavioral_path):
@@ -47,13 +48,29 @@ def summarize_trials(raw_behavioral_path):
     return np.array(trials).astype(float)
 
 
-def get_responses(df, presentation_idx, trials, dep_variable='scaling'):
-    r"""Summarize subject's correct/incorrect as function of dep_variable
+def create_experiment_df(df, presentation_idx, dep_variables=['scaling']):
+    r"""Create a dataframe summarizing the trials of the experiment
 
-    This function returns a dictionary with keys corresponding to the
-    different levels of ``dep_variable`` presented in the experiment and
-    values that are arrays containing 1s and 0s: 1 for every trial the
-    subject was correct, 0 for every trial they were incorrect
+    This function takes in the dataframe summarizing the stimuli, the
+    presentation indices, and some dependent variables, and creates a
+    dataframe summarizing each trial in the experiment. We have the
+    following columns:
+    - 'image_name': the name of the reference image used to generate
+      these images
+    - 'image_1': the seed of the first image presented
+    - 'image_2': the seed of the second image presented
+    - 'image_X': the seed of the third image presented
+    - 'trial_number': the number of this trial
+    - 'correct_response': whether the correct response was 1 (if image_1
+      and image_X were identical) or 2 (if image_2 and image_X were
+      identical)
+    - 'model': the model used to generate this metamer
+    - an additional column for each item in ``dep_variable``
+
+    On some trials, one of the two images presented will be a
+    metamer. In that case, their seed (as stored in ``df``) is
+    ``np.nan``. In ``expt_df`` we replace this with the str
+    ``'reference'``.
 
     Parameters
     ----------
@@ -63,32 +80,131 @@ def get_responses(df, presentation_idx, trials, dep_variable='scaling'):
     presentation_idx : np.array
         The n_trials by 3 array containing the stimuli presentation
         indices for the run being analyzed.
-    trials : np.array
-        The n_trials by 4 array created by analysis.summarize_trials
-    dep_variable : str, optional
-        One of the columns of ``df``, which tells us which variable we
-        want to use as the dependent variable in our psychometric
-        function.
+    dep_variable : list, optional
+        A list of strs, containing one or more of the columns of ``df``,
+        which tells us which additional variable(s) we want to include
+        in our dataframe. These are assumed to be identical in the two
+        images presented on each trial (function will raise an Exception
+        if this not true), and the intended use case if that these will
+        be the dependent variables against which we will plot our
+        psychometric function
 
     Returns
     -------
-    responses : dict
-        The response dictionary, as described above
+    expt_df : pd.DataFrame
+        The experiment information dataframe, see above for description
+
+    """
+    sub_df = df[['image_name']]
+    dep_variables.append('model')
+    correct_answers = np.where(presentation_idx[:, 2] == presentation_idx[:, 0], 1, 2)
+    expt_df = []
+    for i, (a, b, x) in enumerate(presentation_idx):
+        tmp = sub_df.loc[a].to_dict()
+        # these two should be identical
+        if tmp != sub_df.loc[b].to_dict():
+            raise Exception("Something's gone horribly wrong, the identifying info for trial %s "
+                            "is incorrect! %s does not match %s: %s, %s" %
+                            (i, a, b, tmp, sub_df.loc[b].to_dict()))
+        tmp.update({'image_1': df.loc[a].seed, 'image_2': df.loc[b].seed,
+                    'image_X': df.loc[x].seed, 'trial_number': i,
+                    'correct_response': correct_answers[i]})
+        for v in dep_variables:
+            # at most one of these will be nan. they could be strings,
+            # in which case the isinstance call will fail
+            if isinstance(df.loc[a][v], float) and np.isnan(df.loc[a][v]):
+                tmp.update({v: df.loc[b][v]})
+            else:
+                tmp.update({v: df.loc[a][v]})
+                if not isinstance(df.loc[b][v], float) or not np.isnan(df.loc[b][v]):
+                    if df.loc[a][v] != df.loc[b][v]:
+                        raise Exception("Something's gone horribly wrong, dependent variable for "
+                                        "images %s and %s in trial %s don't match: %s, %s" %
+                                        (a, b, i, df.loc[a][v], df.loc[b][v]))
+        expt_df.append(pd.DataFrame(tmp, [i]))
+    expt_df = pd.concat(expt_df).reset_index(drop=True)
+    # all NaNs are where we have a reference image
+    expt_df = expt_df.fillna('reference')
+    # insert information on trial type: metamer vs metamer or metamer vs
+    # reference. if either image is a reference, then this is metamer vs
+    # reference; else, it's metamer vs metamer
+    metamer_vs_reference = np.logical_or((expt_df.image_1 == 'reference').values,
+                                         (expt_df.image_2 == 'reference').values)
+    expt_df['trial_type'] = np.where(metamer_vs_reference, 'metamer_vs_reference',
+                                     'metamer_vs_metamer')
+    return expt_df
+
+
+def add_response_info(expt_df, trials, subject_name):
+    r"""Add information about subject's response and correctness to expt_df
+
+    This function takes the expt_df, which summarizes the trials of the
+    experiment, and adds three additional columns: 'subject_response',
+    which gives the number (1 or 2) the subject pressed on this trial,
+    'hit_or_miss', which contains either 'hit' or 'miss', describing
+    whether the subject was correct or not, and 'subject_name', which
+    contains the name of the subject corresponding to the trials array.
+
+    Parameters
+    ----------
+    expt_df : pd.DataFrame
+        The experiment information dataframe, as created by
+        analysis.create_experiment_df
+    trials : np.array
+        The n_trials by 4 array created by analysis.summarize_trials
+    subject_name : str
+        The name of this subject
+
+    Returns
+    -------
+    expt_df : pd.DataFrame
+        The modified experiment dataframe, with three additional
+        columns. See above for description
 
     """
     # just in case it was an incomplete trial
-    presentation_idx = presentation_idx[:len(trials)]
-    correct_answers = np.where(presentation_idx[:, 2] == presentation_idx[:, 0], 1, 2)
-    subj_answers = trials[:, 2]
-    dep_variable_vals = np.where(~np.isnan(df.loc[presentation_idx[:, 0]][dep_variable]),
-                                 df.loc[presentation_idx[:, 0]][dep_variable],
-                                 df.loc[presentation_idx[:, 1]][dep_variable])
-    subj_correct = (correct_answers == subj_answers).astype(int)
+    expt_df = expt_df.iloc[:len(trials)]
+    subj_answers = trials[:, 2].astype(int)
+    expt_df['subject_response'] = subj_answers
+    expt_df['hit_or_miss'] = np.where(expt_df.correct_response == expt_df.subject_response, 'hit',
+                                      'miss')
+    expt_df['subject_name'] = subject_name
+    return expt_df
 
-    responses = {}
-    for k in df[dep_variable].unique():
-        if np.isnan(k):
-            continue
-        dep_variable_trials = np.where(dep_variable_vals[:5] == k)
-        responses[k] = subj_correct[dep_variable_trials]
-    return responses
+
+def summarize_expt(expt_df, dep_variables=['scaling', 'trial_type']):
+    r"""Summarize expt_df to get proportion correct
+
+    Here, we take the ``expt_df`` summarizing the experiment's trials
+    and the subject's responses, and we compute the proportion correct
+    on each trial type. We end up with a DataFrame that has the columns
+    ``['subject_name', 'image_name', 'model'] + dep_variables`` from
+    ``expt_df``, as well as two new columns: ``'n_trials'`` (which gives
+    the number of trials in that condition) and ``'proportion_correct'``
+    (which gives the proportion of time the subject was correct in that
+    condition).
+
+    Parameters
+    ----------
+    expt_df : pd.DataFrame
+        The experiment information dataframe, after modification by
+        analysis.add_response_info
+    dep_variable : list, optional
+        A list of strs, containing one or more of the columns of
+        ``expt_df``, which tells us which additional variable(s) we want
+        to include in our dataframe.
+
+    Returns
+    -------
+    summary_df : pd.DataFrame
+
+    """
+    expt_df = expt_df.copy()
+    expt_df['hit_or_miss'] = expt_df.hit_or_miss.apply(lambda x: {'hit': 1, 'miss': 0}[x])
+
+    gb = expt_df.groupby(['subject_name', 'image_name', 'model'] + dep_variables)
+    summary_df = gb.count()['image_1'].reset_index()
+    summary_df = summary_df.merge(gb.hit_or_miss.mean().reset_index())
+    summary_df = summary_df.rename(columns={'image_1': 'n_trials',
+                                            'hit_or_miss': 'proportion_correct'})
+    return summary_df
