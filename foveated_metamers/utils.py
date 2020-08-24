1031174a2d18b2c1321b715c91ebb75ca2e920cc
William F. Broderick <billbrod@gmail.com>
2020-08-24 16:17:55 -0500

    utils.py should also constrain loss_change_iter to int

--- foveated_metamers/utils.py
+++ foveated_metamers/utils.py
@@ -360,7 +360,7 @@ if __name__ == '__main__':
             continue
         if k == 'DATA_DIR':
             new_args[k] = v
-        elif k in ['seed', 'max_iter', 'gpu']:
+        elif k in ['seed', 'max_iter', 'gpu', 'loss_change_iter']:
             # then it's an int
             new_args[k] = [int(vi) for vi in v]
         else:
