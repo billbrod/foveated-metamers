8c8e873750fab3e013b8ae384c5fcd92f53f01ad
William F. Broderick <billbrod@gmail.com>
2020-10-09 17:38:04 -0400

    don't show countdown when quitting

--- foveated_metamers/experiment.py
+++ foveated_metamers/experiment.py
@@ -391,17 +391,19 @@ def run(stimuli_path, idx_path, save_path, on_msec_length=200, off_msec_length=(
             paused_keys = pause(i+1, len(stimuli), win, img_pos, expt_clock, flip_text)
             timings.append(('pause', 'stop', expt_clock.getTime()))
             keys_pressed.extend(paused_keys)
-            countdown(win, img_pos, flip_text, text_height)
+            if not check_for_keys(paused_keys):
+                countdown(win, img_pos, flip_text, text_height)
         else:
             paused_keys = []
         save(save_path, stimuli_path, idx_path, keys_pressed, timings, expt_params, idx,
              screen=screen, edf_path=edf_path, screen_size_deg=screen_size_deg,
              last_trial=i+start_from_stim, **monitor_kwargs)
-        [f.draw() for f in fixation]
-        [w.flip() for w in win]
-        core.wait(.5)
         if check_for_keys(all_keys+paused_keys):
             break
+        else:
+            [f.draw() for f in fixation]
+            [w.flip() for w in win]
+            core.wait(.5)
     [visual.TextStim(w, "Run over", pos=p, flipHoriz=flip_text, height=text_height).draw() for w, p in zip(win, img_pos)]
     [w.flip() for w in win]
     timings.append(("run_end", '', expt_clock.getTime()))
